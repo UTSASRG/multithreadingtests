4e74af806e8786f4e5d50cd006e7f40a9a36e379
xinnnnzhao <xinnnnzhao@gmail.con>
2020-01-21 17:11:12 -0500

    work in new machine

--- parsec/tests/facesim/Public_Library/obj/Utilities/DEBUG_UTILITIES.d
+++
@@ -1,7 +0,0 @@
-obj/Utilities/DEBUG_UTILITIES.o obj/Utilities/DEBUG_UTILITIES.d: \
- Utilities/DEBUG_UTILITIES.cpp Utilities/DEBUG_UTILITIES.h \
- Utilities/../Utilities/LOG.h
-
-Utilities/DEBUG_UTILITIES.h:
-
-Utilities/../Utilities/LOG.h:

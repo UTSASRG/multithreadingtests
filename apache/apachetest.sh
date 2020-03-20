683062968d6500faaf138f59b7fec0c032d5944a
tongping <tongping@umass.edu>
2020-03-19 20:33:13 -0500

    apach test

--- apache/apachetest.sh
+++ apache/apachetest.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-./apache2/bin/httpd -k start
-./apache2/bin/ab -n 100000 -c 100 http://192.168.1.2:1978/
-./apache2/bin/httpd -k stop
+./httpd-2.4.23/install/bin/httpd -k start
+#./apache2/bin/ab -n 100000 -c 100 http://192.168.1.2:1978/
+./httpd-2.4.23/install/bin/ab -n 100000 -c 100 http://10.242.129.222:1978/
+./httpd-2.4.23/install/bin/httpd -k stop

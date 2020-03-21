f18df21e854a8bed70e10db1a51b8b3eb6f62492
xinnnnzhao <xinnnnzhao@gmail.com>
2020-03-20 22:32:11 -0500

    apache test

--- apache/remote_test.sh
+++ apache/remote_test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 for allocator in {"pthread","numalloc","tcmalloc","jemalloc","tbbmalloc","scalloc"}
+#for allocator in {"scalloc",}
 do
   for i in `seq 1`
   do
     echo "begin build $allocator"
     ssh tpliu@dynaoptimal950.cs.utsa.edu "cd /home/tpliu/xinzhao/multithreadingtests/apache;./build.sh $allocator" > /dev/null
-    echo "begin test $allocator---$i"
+    echo "begin test $allocator---$i:" >> 'test.out'
     ssh tpliu@dynaoptimal950.cs.utsa.edu "cd /home/tpliu/xinzhao/multithreadingtests/apache;./httpd.sh start $allocator"
-    time ./httpd-2.4.23/install/bin/ab -n 1000000 -c 1000 http://10.242.129.222:1978/
-    ssh tpliu@dynaoptimal950.cs.utsa.edu "cd /home/tpliu/xinzhao/multithreadingtests/apache;./httpd.sh stop $allocator"
+    /usr/bin/time -a -o 'test.out' ./httpd-2.4.23/install/bin/ab -n 1000000 -c 1000 http://10.242.129.222:1978/
+    ssh tpliu@dynaoptimal950.cs.utsa.edu "cd /home/tpliu/xinzhao/multithreadingtests/apache;./httpd.sh stop $allocator" >> 'test.out'
   done
 done

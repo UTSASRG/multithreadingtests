aa75b663666b1610d09d4d0448da41368d63e8f3
xinnnnzhao <xinnnnzhao@gmail.com>
2020-03-21 22:22:43 -0500

    remote server test

--- apache/remote_test.sh
+++ apache/remote_test.sh
@@ -3,10 +3,10 @@
 for allocator in {"pthread","numalloc","tcmalloc","jemalloc","tbbmalloc","scalloc"}
 #for allocator in {"scalloc",}
 do
+  echo "begin build $allocator"
+  ssh tpliu@dynaoptimal950.cs.utsa.edu "cd /home/tpliu/xinzhao/multithreadingtests/apache;./build.sh $allocator" > /dev/null
   for i in `seq 1`
   do
-    echo "begin build $allocator"
-    ssh tpliu@dynaoptimal950.cs.utsa.edu "cd /home/tpliu/xinzhao/multithreadingtests/apache;./build.sh $allocator" > /dev/null
     echo "begin test $allocator---$i:" >> 'test.out'
     ssh tpliu@dynaoptimal950.cs.utsa.edu "cd /home/tpliu/xinzhao/multithreadingtests/apache;./httpd.sh start $allocator"
     /usr/bin/time -a -o 'test.out' ./httpd-2.4.23/install/bin/ab -n 1000000 -c 1000 http://10.242.129.222:1978/
--- memcached/remote_test.sh
+++ memcached/remote_test.sh
@@ -3,10 +3,10 @@ set -x
 for allocator in {"pthread","numalloc","tcmalloc","jemalloc","tbbmalloc","scalloc"}
 #for allocator in {"scalloc",}
 do
+  echo "begin build $allocator"
+  ssh tpliu@dynaoptimal950.cs.utsa.edu "cd /home/tpliu/xinzhao/multithreadingtests/memcached;./build.sh $allocator" > /dev/null
   for i in `seq 1`
   do
-    echo "begin build $allocator"
-    ssh tpliu@dynaoptimal950.cs.utsa.edu "cd /home/tpliu/xinzhao/multithreadingtests/memcached;./build.sh $allocator" > /dev/null
     echo "begin test $allocator---$i:" >> 'test.out'
     ssh tpliu@dynaoptimal950.cs.utsa.edu "cd /home/tpliu/xinzhao/multithreadingtests/memcached;./memcached.sh start $allocator" &
     sleep 5

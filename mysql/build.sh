3acd537bf96ebe641020031d82a01f9ee0c902e2
xinnnnzhao <xinnnnzhao@gmail.com>
2020-03-26 00:48:02 -0500

    mysql test

--- mysql/build.sh
+++ mysql/build.sh
@@ -7,52 +7,39 @@ set -x
     exit 1
   fi
 
-  rm -rf 'mysql-5.7.15/'
-  rm -rf 'sysbench'
-  
-  #config_vars=config_vars.mk
+  #rm -rf 'mysql-5.7.15/'
+  #rm -rf 'sysbench'
+  #
+  ## install sysbench
+  #tar -xvf sysbench.tar.gz
+  #cd sysbench
+  #./configure
+  #make
+  #cd ../
 
-  #if [ $# == 1 ]; then
-  #  config_vars=$config_vars.$1
+  ## install mysql
+  #tar zxvf mysql-boost-5.7.15.tar.gz
+  #cd mysql-5.7.15/
+  #cmake -DWITH_BOOST=boost .
+
+  #if [ $# != 0 ] && [ $1 != "pthread" ]; then
+  #  mv "$home/mysql/mysql-5.7.15/sql/CMakeFiles/mysqld.dir/link.txt" "$home/mysql/mysql-5.7.15/sql/CMakeFiles/mysqld.dir/link.txt.bk"
+  #  cat "$home/mysql/mysql-5.7.15/sql/CMakeFiles/mysqld.dir/link.txt.bk" | sed "s;\-lpthread;-rdynamic ${lib_with_path_map[$1]} -lpthread;g" > "$home/mysql/mysql-5.7.15/sql/CMakeFiles/mysqld.dir/link.txt"
   #fi
 
-  #if [ ! -f $config_vars ]; then
-  #  echo "$config_vars does not exists" 
-  #  exit 1
-  #fi
-
-  # install sysbench
-  tar -xvf sysbench.tar.gz
-  cd sysbench
-  ./configure
-  make
-  cd ../
+  #make
+  #make install DESTDIR="$home/mysql/mysql-5.7.15/install"
 
 
-
-  # install mysql
-  tar zxvf mysql-boost-5.7.15.tar.gz
-  cd mysql-5.7.15/
-  cmake -DWITH_BOOST=boost .
-
-  if [ $# != 0 ] && [ $1 != "pthread" ]; then
-    mv "$home/mysql/mysql-5.7.15/sql/CMakeFiles/mysqld.dir/link.txt" "$home/mysql/mysql-5.7.15/sql/CMakeFiles/mysqld.dir/link.txt.bk"
-    cat "$home/mysql/mysql-5.7.15/sql/CMakeFiles/mysqld.dir/link.txt.bk" | sed "s;\-lpthread;-rdynamic ${lib_with_path_map[$1]} -lpthread;g" > "$home/mysql/mysql-5.7.15/sql/CMakeFiles/mysqld.dir/link.txt"
-  fi
-
-  make
-  make install DESTDIR="$home/mysql/mysql-5.7.15/install"
-
-
-  # inite mysql 
+  ## inite mysql 
   cd "$home/mysql/mysql-5.7.15/install/usr/local/mysql/"
-  ./bin/mysqld --initialize --user=tpliu --datadir="$home/mysql/mysql-5.7.15/install/usr/local/mysql/data" --basedir="$home/mysql/mysql-5.7.15/install/usr/local/mysql"
+  #./bin/mysqld --initialize --user=tpliu --datadir="$home/mysql/mysql-5.7.15/install/usr/local/mysql/data" --basedir="$home/mysql/mysql-5.7.15/install/usr/local/mysql"
 
 
   #create test databases & inite test data
   bin/mysqld_safe --user=$user &
   sleep 5
   mysql -u root -p11 -S /tmp/mysql.sock < "$home/mysql/create_database.sql"
-  $home/mysql/sysbench/sysbench/sysbench "--test=$home/mysql/sysbench/sysbench/tests/db/oltp.lua --oltp-table-size=10000 --mysql-socket=/tmp/mysql.sock --mysql-user=root --mysql-password=11 prepare"
-  bin/mysqladmin shutdown -u root -p11 -S/tmp/mysql.sock
+  $home/mysql/sysbench/sysbench/sysbench --test=$home/mysql/sysbench/sysbench/tests/db/oltp.lua --oltp-table-size=10000 --mysql-socket=/tmp/mysql.sock --mysql-user=root --mysql-password=11 prepare
+  #bin/mysqladmin shutdown -u root -p11 -S/tmp/mysql.sock
   sleep 5

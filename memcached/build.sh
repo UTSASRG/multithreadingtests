7987f5621194028b88bc15baa34c9cd610486a1a
xinnnnzhao <xinnnnzhao@gmail.com>
2020-03-27 18:50:40 -0500

    memcached test

--- memcached/build.sh
+++ memcached/build.sh
@@ -17,7 +17,11 @@
   tar xvf memcached-1.4.25.tar
   cd memcached-1.4.25/
   ./configure --prefix=$home/memcached/memcached-1.4.25/install --enable-61bit --with-libevent=$home/memcached/libevent-2.1.8-stable/install/
-  
-  cp ../$config_vars ./Makefile
+  cat Makefile | sed 's/\-Werror\ //' > Makefile.bk
+  cp Makefile.bk Makefile
+  if [ $# != 0 ] && [ "${lib_with_path_map[$1]}" != "NULL" ]; then
+    cat Makefile.bk | sed "s;\-pthread;-rdynamic ${lib_with_path_map[$1]} -pthread;" > Makefile
+  fi
+  #cp ../$config_vars ./Makefile
   make
   make install

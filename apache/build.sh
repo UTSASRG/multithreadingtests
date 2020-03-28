55856cd2daaa1e00616ef7ad4ec48d910aba2367
xinnnnzhao <xinnnnzhao@gmail.com>
2020-03-27 20:18:34 -0500

    apache test

--- apache/build.sh
+++ apache/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-  rm -rf 'httpd-2.4.23'
+  rm -rf 'httpd-2.4.35'
   
   config_vars=config_vars.mk
 
@@ -12,14 +12,15 @@
     echo "$config_vars does not exists" 
     exit 1
   fi
-  tar -xvf httpd-2.4.23.tar.gz
+  #tar -xvf httpd-2.4.23.tar.gz
+  tar -xvf httpd-2.4.35.tar.gz
   tar -xvf apr-1.7.0.tar.gz
   tar -xvf apr-util-1.6.1.tar.gz
-  mv apr-util-1.6.1 httpd-2.4.23/srclib/apr-util
-  mv apr-1.7.0 httpd-2.4.23/srclib/apr
+  mv apr-util-1.6.1 httpd-2.4.35/srclib/apr-util
+  mv apr-1.7.0 httpd-2.4.35/srclib/apr
  # mv httpd-2.4.23 httpd-2.4.23.$i
-  cd httpd-2.4.23
-  ./configure --with-included-apr --prefix=/home/tpliu/xinzhao/multithreadingtests/apache/httpd-2.4.23/install
+  cd httpd-2.4.35
+  ./configure --with-included-apr --prefix=/home/tpliu/xinzhao/multithreadingtests/apache/httpd-2.4.35/install
   cp ../$config_vars ./build/config_vars.mk
   make
   make install

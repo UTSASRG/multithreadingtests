8bf8b5ba555af7655c3f6dcd19d67ca63fe855ff
xinnnnzhao <xinnnnzhao@gmail.com>
2020-03-28 22:38:19 -0400

    apache test

--- apache/build.sh
+++ apache/build.sh
@@ -2,16 +2,16 @@
   set -x
   source ../home_var.sh
 
-  rm -rf 'httpd-2.4.23'
+  rm -rf 'httpd-2.4.35'
 
-  tar -xvf httpd-2.4.23.tar.gz
+  tar -xvf httpd-2.4.35.tar.gz
   tar -xvf apr-1.7.0.tar.gz
   tar -xvf apr-util-1.6.1.tar.gz
-  mv apr-util-1.6.1 httpd-2.4.23/srclib/apr-util
-  mv apr-1.7.0 httpd-2.4.23/srclib/apr
- # mv httpd-2.4.23 httpd-2.4.23.$i
-  cd httpd-2.4.23
-  ./configure --with-included-apr --prefix=$home/apache/httpd-2.4.23/install
+  mv apr-util-1.6.1 httpd-2.4.35/srclib/apr-util
+  mv apr-1.7.0 httpd-2.4.35/srclib/apr
+ # mv httpd-2.4.35 httpd-2.4.35.$i
+ cd httpd-2.4.35
+  ./configure --with-included-apr --prefix=$home/apache/httpd-2.4.35/install
  # cp ../$config_vars ./build/config_vars.mk
   if [ $# != 0 ] && [ "${lib_with_path_map[$1]}" != "NULL" ]; then
     mv ./build/config_vars.mk ./build/config_vars.mk.bk
@@ -19,4 +19,4 @@
   fi
   make
   make install
-  cp ../httpd.conf install/conf/
+  cat ../httpd.conf | sed "s/#user#/$user/" | sed "s/#local_ip#/$local_ip/" | sed "s;#home#;$home;" > install/conf/httpd.conf

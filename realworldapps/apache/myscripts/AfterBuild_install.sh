62ad137a62e2c7cd7d7f84febf012104c5d7992c
Steven Tang <mrsteventang@outlook.com>
2021-04-14 21:54:03 -0400

    Move build script to example

--- realworldapps/apache/myscripts/AfterBuild_install.sh
+++ realworldapps/apache/myscripts/AfterBuild_install.sh
@@ -1,18 +1,1 @@
-#!/bin/bash
-
-#Print commands and their arguments while this script is executed
-# set -x
-
-echo "Checking parameters"
-
-if (( $# < 1 ))
-then
-  echo "Usage: AfterBuild_install BUILD_NAME (This BUILD_NAME is passed to all scripts. And we'll install compiled binaries under $BUILD_NAME folder"
-  exit 1
-fi
-
-cd $INSTALLATION_FOLDER
-
-echo "Changing listening ip and port to $APACHE_LISTENING_IP:$APACHE_LISTENING_PORT as instructed"
-cp ./conf/httpd.conf ./conf/httpd.conf.bkp
-cat ./conf/httpd.conf.bkp | sed "s/^Listen 80/Listen $APACHE_LISTENING_IP:$APACHE_LISTENING_PORT/" > ./conf/httpd.conf
+../examplescripts/mmprof/AfterBuild_install.sh

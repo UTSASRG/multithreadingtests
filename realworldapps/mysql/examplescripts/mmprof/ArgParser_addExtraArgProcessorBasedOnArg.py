1b1700b750f2b94427f07364d3716e2de43f0c0a
Steven Tang <mrsteventang@outlook.com>
2021-04-15 17:52:05 -0400

    Fix indention error

--- realworldapps/mysql/examplescripts/mmprof/ArgParser_addExtraArgProcessorBasedOnArg.py
+++ realworldapps/mysql/examplescripts/mmprof/ArgParser_addExtraArgProcessorBasedOnArg.py
@@ -24,26 +24,21 @@ buildCommand = buildCommand[0].strip()
 #Split build arguments by space
 argV = sys.argv
 
-MY_ARTIFECTS_DIR=os.environ['BENCHMARK_ROOT_DIR']+'/myartifects'
+MY_ARTIFECTS_DIR = os.environ['BENCHMARK_ROOT_DIR']+'/myartifects'
 memoryAllocatorsLibPath = {"hoard": MY_ARTIFECTS_DIR+"/libhoard.so",
                            "libc221": MY_ARTIFECTS_DIR+"/libmalloc221.so",
-                           "libc228":MY_ARTIFECTS_DIR+"/libmalloc228.so",
-                           "tcmalloc":MY_ARTIFECTS_DIR+"/libtcmalloc_minimal.so",
-                           "jemalloc":MY_ARTIFECTS_DIR+"/libjemalloc.so"}
+                           "libc228": MY_ARTIFECTS_DIR+"/libmalloc228.so",
+                           "tcmalloc": MY_ARTIFECTS_DIR+"/libtcmalloc_minimal.so",
+                           "jemalloc": MY_ARTIFECTS_DIR+"/libjemalloc.so"}
 
-if (len(argV) ==3):
-   if(argV[2].startswith("mmprof_NOUTIL")):
-      mmprofPath=MY_ARTIFECTS_DIR+"/libmallocprof_noutil.so"
-      buildArgList.insert('-rdynamic ')
-      buildArgList.append(mmprofPath+" ")
+
+if (len(argV) == 3):
+    if(argV[2].startswith("mmprof_NOUTIL")):
+      mmprofPath = MY_ARTIFECTS_DIR+"/libmallocprof_noutil.so"
     elif (argV[2].startswith("mmprof_UTIL")):
-      mmprofPath=MY_ARTIFECTS_DIR+"/libmallocprof_util.so"
-      buildArgList.append('-rdynamic ')
-      buildArgList.append(mmprofPath+" ")
+      mmprofPath = MY_ARTIFECTS_DIR+"/libmallocprof_util.so"
     elif (argV[2].startswith("mmprof_MALLOCNUM")):
-      mmprofPath=MY_ARTIFECTS_DIR+"/libmallocprof_mallocnum.so"
-      buildArgList.append('-rdynamic ')
-      buildArgList.append(mmprofPath+" ")
+      mmprofPath = MY_ARTIFECTS_DIR+"/libmallocprof_mallocnum.so"
     else:
       print("mmprof has two versions: mmprof_UTIL and mmprof_NOUTIL", file=sys.stderr)
       sys.exit(-1)
@@ -51,11 +46,13 @@ if (len(argV) ==3):
 #Map memory allocator with the first argument. I susppose there are only one argument. And it must be the name of an allocator
 #Map memory allocator with the first argument. I susppose there are only one argument. And it must be the name of an allocator
 if(len(argV) == 2 and (not argV[1] in memoryAllocatorsLibPath)):
-    print("You need to pass one and only one allocator name to build.sh", file=sys.stderr)
-    print("Configured allocators:\n" + str(memoryAllocatorsLibPath), file=sys.stderr)
+    print("You need to pass one and only one allocator name to build.sh",
+          file=sys.stderr)
+    print("Configured allocators:\n" +
+          str(memoryAllocatorsLibPath), file=sys.stderr)
     print("Current Arguments:\n" + str(sys.argv), file=sys.stderr)
     sys.exit(-1)
-elif (len(argV) ==3 and not( argV[2].startswith("mmprof"))):
+elif (len(argV) == 3 and not(argV[2].startswith("mmprof"))):
    print("If you to link mmprof. The third parameter would have to be mmprof", file=sys.stderr)
    print("Current Arguments:\n" + str(sys.argv), file=sys.stderr)
    sys.exit(-1)
@@ -68,13 +65,16 @@ elif len(argV) > 3:
 #Dynamically select build arg
 extraBuildArgs = None
 if(sys.argv[-1].startswith('mmprof')):
-    extraBuildArgs = " -rdynamic "+mmprofPath+" -rdynamic " + memoryAllocatorsLibPath[argV[1]]
+    extraBuildArgs = " -rdynamic "+mmprofPath + \
+        " -rdynamic " + memoryAllocatorsLibPath[argV[1]]
 else:
     extraBuildArgs = " -rdynamic "+memoryAllocatorsLibPath[argV[1]]
 
-insertLoc=buildCommand.find('-O3 -g ')
+#Make sure mallocperf is inserted in front of other libraries
+insertLoc = buildCommand.find('-O3 -g ')
 
-buildCommand = buildCommand[0:insertLoc]+" "+extraBuildArgs+" "+buildCommand[insertLoc:]
+buildCommand = buildCommand[0:insertLoc]+" " + \
+    extraBuildArgs+" "+buildCommand[insertLoc:]
 
 # Put final command to stdout. You shouldn't print anything else to stdout.
 # Otherwide after I/O redirection you will get wrong result.
